ba395f45ae2184c1da2688c9df92303e77f9bc03
Fumikazu Konishi <fumi1ko24@gmail.com>
2022-04-15 10:32:00 +0900

    add tljh-quickstart script

---
+++ publicscript/tljh-quickstart.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERNAME=${USERNAME:-"admin"}
+INTERFACE=${INTERFACE:-""}
+AUTHTYPE=${AUTHTYPE:-""}
+FAILED_LOGINS=${FAILED_LOGINS:-5}
+NEXT_TRY=${NEXT_TRY:-120}
+
+curl -L https://tljh.jupyter.org/bootstrap.py \
+  | sudo python3 - \
+    --admin $USERNAME
+
+
+if [$AUTHTYPE == "NativeAuthenticator"]; then
+    tljh-config set auth.type nativeauthenticator.NativeAuthenticator
+    tljh-config set auth.NativeAuthenticator.allowed_failed_logins $FAILED_LOGINS
+    tljh-config set auth.NativeAuthenticator.seconds_before_next_try $NEXT_TRY
+
+cat <<'EOT'>/opt/tljh/config/jupyterhub_config.d/custom_template.py
+import os, nativeauthenticator
+c.JupyterHub.template_paths = [f"{os.path.dirname(nativeauthenticator.__file__)}/templates/"]
+EOT
+
+fi
+
+if [$INTERFACE != ""]; then
+    tljh-config set user_environment.default_app $INTERFACE
+fi
+    
+
+tljh-config reload
